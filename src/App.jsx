269095cb49824a39ba2be54f90bd28c39adadfb1
DexterYBN <yakubov959@mail.ru>
2022-11-29 10:56:18 +0300

    success

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import Shoes from './Database';
+import { useState } from 'react';
+import './styles.css';
+
+
+function App() {
+
+  const [search, setSearch] = useState("")
+
+  const [product, setProduct] = useState(Shoes)
+
+  // const [blur, setBlur] = useState("")
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value)
+    setProduct(
+      Shoes.filter((item) => {
+        return item.name.toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase())
+      })
+    )
+  }
+
+  // const handleBlur = () => {
+  //   setBlur()
+  // }
+
+  return (
+    <>
+      <div className='header'>
+        <div class="input-group mb-3">
+          <span class="input-group-text" id="inputGroup-sizing-default">Search</span>
+          <input
+            onChange={handleSearch}
+            value={search}
+            // onBlur={handleBlur}
+            type="text"
+            class="form-control"
+            aria-label="Пример размера поля ввода"
+            aria-describedby="inputGroup-sizing-default" />
+        </div>
+      </div>
+      <div className="App">
+        {product.map((item) => {
+          return (
+            <>
+              <div className='main'>
+                <div class="shadow p-3 mb-5 bg-body rounded">
+                  <div className='card'>
+                    <img key={item.id} className='images' src={item.img} alt={item.name} />
+                    <div className='nameNprice'>
+                      <div className='names'>{item.name}</div>
+                      <div className='btnDiv'>
+                        <button className='btnButton'>{item.details}</button>
+                      </div>
+                      <div className='price'>{item.price}</div>
+                    </div>
+                    <div>
+                      <hr className="line" />
+                    </div>
+                    <div className='cardFooter'>
+                      <p className='footerText'>{item.info}</p>
+                    </div>
+                    <button type="button" class="btn btn-success">Добавить в корзину</button>
+                  </div>
+                </div>
+              </div>
+            </>
+          )
+        })}
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Database.jsx
@@ -0,0 +1,65 @@
+import adidas from './images/adidas.jpg'
+import convers from './images/convers.jpeg'
+import iziki from './images/izik.jpg'
+import nike from './images/nike.jpg'
+import puma from './images/puma.jpeg'
+import tufli from './images/tufli.jpg'
+
+const Shoes = [
+
+    {
+        id: 1,
+        name: "Adidas",
+        img: adidas,
+        price: " $200",
+        details: "Детали",
+        info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit"
+    },
+
+    {
+        id: 2,
+        name: "Convers",
+        img: convers,
+        price: " $1231",
+        details: "Детали",
+        info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit"
+    },
+
+    {
+        id: 3,
+        name: "YEEZY BOOST 350 V2",
+        img: iziki,
+        price: " $300",
+        details: "Детали",
+        info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit"
+    },
+
+    {
+        id: 4,
+        name: "Nike",
+        img: nike,
+        price: " $120",
+        details: "Детали",
+        info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit"
+    },
+
+    {
+        id: 5,
+        name: "Puma",
+        img: puma,
+        price: " $56",
+        details: "Детали",
+        info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit"
+    },
+
+    {
+        id: 6,
+        name: "Shoe",
+        img: tufli,
+        price: " $100",
+        details: "Детали",
+        info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit"
+    }
+]
+
+export default Shoes;
